cb15e6670795df4c061a5f279b422f5f441738d0
David Preciado <60233384+almostvenice@users.noreply.github.com>
2025-06-14 16:23:02 -0400

    update iot

--- AutoHotkey/WindowsScripts/kasaTest.py
+++ AutoHotkey/WindowsScripts/kasaTest.py
@@ -1,8 +1,9 @@
 import asyncio
-from kasa import SmartPlug
+from kasa.iot import IotPlug
 
 async def main():
-    plug = SmartPlug("192.168.5.132")
+    plug = IotPlug("192.168.5.132")
+    await plug.connect()
     await plug.update()  # Fetch current state
     print(f"Current state: {plug.is_on}")
     # await plug.turn_on()  # Turn on the plug
